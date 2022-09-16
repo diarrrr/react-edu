8bea371e65c8442520b6efae7dcc3e6b3e5b3b3e
Diar <109844358+diarrrr@users.noreply.github.com>
2022-09-16 21:52:22 +0700

    tugas 1

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+const header = () => {
+    return (
+        <div>
+            <ul>
+                <li> HOME </li>
+                <li> CONTACT </li>
+                <li> ABOUT </li>
+            </ul>
+        </div>
+    )
+};
+
+export default header;
---
+++ src/components/myButton.js
@@ -0,0 +1,9 @@
+const Button = (props) =>{
+    return (
+        <div>
+            <button onClick={() => props.tekan()}>CLICK ME!!!</button>
+        </div>
+    )
+};
+
+export default (Button);
